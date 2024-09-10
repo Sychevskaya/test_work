748fbd01a09b837574b55b4cee4687665be88546
Sychevskaya <liza.sychevskaya@bk.ru>
2024-09-10 23:21:18 +0300

    TEST

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.main-slider_1').slick({
+        arrows: false,
+        autoplay: true,
+        slidesToShow: 1,
+        autoplaySpeed: 2000,
+      });
+  });
